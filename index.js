f573e53f57c51fa84308c5e3bec443559564d1d7
GT <87564176+GT-X1@users.noreply.github.com>
2021-08-04 02:49:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -461,7 +461,6 @@ async function starts() {
 			const isGroupAdmins = groupAdmins.includes(sender) || false
 			const isWelkom = isGroup ? welkom.includes(from) : false
 			const isNsfw = isGroup ? nsfw.includes(from) : false
-			const isSimi = isGroup ? samih.includes(from) : false
 			const isOwner = ownerNumber.includes(sender)
                         const isUser = user.includes(sender)
                         const isLevelingOn = isGroup ? _leveling.includes(groupId) : false
